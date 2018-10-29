5c81afeb91a8ee0755b91ad02b92d26e7a2a3621
Ahmed Mohamed El-Desouky <ahmedmohamedel-desouky@192.168.160.128>
2018-10-29 11:39:08 +0200

    first commit

---
+++ app-swoosh/app-swoosh/Views/MyBtn.swift
@@ -0,0 +1,21 @@
+//
+//  rb.swift
+//  app-swoosh
+//
+//  Created by Ahmed Mohamed El-Desouky on 10/29/18.
+//  Copyright © 2018 Ahmed Mohamed El-Desouky. All rights reserved.
+//
+
+import UIKit
+
+class rb: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
